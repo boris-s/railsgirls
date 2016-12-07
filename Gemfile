a0f4ceb44f5cbcfe85283368824b9521b7c19a28
Boris Stitnicky <borisstitnicky@gmail.com>
2016-12-07 16:33:25 +0100

    Set up devise

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'carrierwave'
 gem 'cloudinary'
 
+gem 'devise'
+
 group :development, :test do
   gem 'sqlite3', '~> 1.3.11'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,10 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Line added when I started using devise.
+  config.action_mailer.default_url_options =
+    { host: 'https://obscure-plateau-62958.herokuapp.com' }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
