2e6e26b34b85690fd83e4dfe4cd500035508e8d3
Boris Stitnicky <borisstitnicky@gmail.com>
2016-12-08 15:32:49 +0100

    Use email/username login in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,24 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  attr_accessor :username
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
+  attr_accessor :login
+  validates :username, presence: true,
+            uniqueness: { case_insensitive: false }
 
   # Vazby na idea a comment + co s nimi po user.destroy
   has_many :ideas, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    conditions[:email].downcase! if conditions[:email]
+    login = conditions.delete :login
+    candidates = where( conditions.to_hash )
+    return candidates.first unless login
+    candidates
+      .where( ["lower(username) = :value OR lower(email) = :value",
+               { value: login.downcase }] )
+      .first
+  end
 end
